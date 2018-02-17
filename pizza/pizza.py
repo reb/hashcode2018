67a7048c0f961c11a468fc21869a24c0814576a3
reb <reboumans@gmail.com>
2018-02-17 14:20:48 +0100

    Add valid_slice

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -6,6 +6,30 @@ def solve(problem):
 
 
 
+def valid_slice(problem, slice_rectangle):
+    r1 = slice_rectangle[0]
+    c1 = slice_rectangle[1]
+    r2 = slice_rectangle[2]
+    c2 = slice_rectangle[3]
+
+    tomatoes = 0
+    mushrooms = 0
+    for r in range(r1, r2):
+        for c in range(c1, c2):
+            if problem['pizza'][r][c] == 'T':
+                tomatoes += 1
+            if problem['pizza'][r][c] == 'M':
+                mushrooms += 1
+
+    enough_tomatoes = tomatoes >= problem['minimum_ingredient']
+    enough_mushrooms = mushrooms >= problem['minimum_ingredient']
+    slice_not_too_big = (tomatoes + mushrooms) <= problem['max_cells']
+
+    if enough_tomatoes and enough_mushrooms and slice_not_too_big:
+        return True
+
+    return False
+            
 
 if __name__ == "__main__":
     datasets = ['example', 'small', 'medium', 'big']
--- pizza/util.py
+++ pizza/util.py
@@ -6,11 +6,11 @@ def load_file(filename):
     with open(filename) as f:
         lines = f.read().splitlines()
 
-        config = lines[0]
-        result["rows"] = config[0]
-        result["columns"] = config[1]
-        result["minimum_ingredient"] = config[2]
-        result["max_cells"] = config[3]
+        config = lines[0].split(' ')
+        result["rows"] = int(config[0])
+        result["columns"] = int(config[1])
+        result["minimum_ingredient"] = int(config[2])
+        result["max_cells"] = int(config[3])
 
         pizza = []
         for line in lines[1:]:
